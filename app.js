95f23a9a5a20440e70a7dd32979cfb2d22b709b5
Suraj9995 <djsurajkuchekar@gmail.com>
2022-06-09 16:45:25 +0530

    update function

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+let screen = document.getElementById("screen");
+
 function screenClear(){
-    document.getElementById("screen").value = "";
+    screen.value = "";
 }
 
 function clickValue(value){
-    document.getElementById("screen").value += value;
+    screen.value += value;
 }
 
 function calculate() {
-    var p = document.getElementById("screen").value;
-    var q = eval(p);
-    document.getElementById("screen").value = q;
-   }
+    var totalValue = eval(screen.value);
+    screen.value = totalValue;
+}
